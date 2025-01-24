023993e21635c3c9b57376423cf3f9ac4b525472
jamistoso <amistoso.juancarlos@gmail.com>
2025-01-24 11:55:44 -0800

    Add thumbnail upload function

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -31,7 +37,84 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse thumbnail", err)
+		return
+	}
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve file", err)
+		return
+	}
+
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse media type", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User does not own video", err)
+		return
+	}
+
+	extension, err := convertMediaTypeToExtension(mediaType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't convert media type to extension", err)
+		return
+	}
+
+	filePath := filepath.Join("./assets/", fmt.Sprintf("%s%s", videoID.String(), extension))
+	newFile, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create file on disk", err)
+		return
+	}
+	
+	_, err = io.Copy(newFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't copy file to disk", err)
+		return
+	}
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/assets/%s%s", cfg.port, videoID, extension)
+	
+	updatedVideo := database.Video{
+		ID: videoID,
+		CreatedAt: video.CreatedAt,
+		UpdatedAt: time.Now(),
+		ThumbnailURL: &thumbnailURL, 
+		VideoURL: video.VideoURL,     
+		CreateVideoParams: database.CreateVideoParams{
+			Title: video.Title,
+			Description: video.Description,
+			UserID: userID,
+		},
+	}
+	err = cfg.db.UpdateVideo(updatedVideo)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, updatedVideo)
+}
+
+func convertMediaTypeToExtension(mediaType string) (string, error) {
+	if mediaType == "image/png"{
+		return ".png", nil
+	} else if mediaType == "image/jpeg"{
+		return ".jpg", nil
+	}
+	return "", fmt.Errorf("invalid media type")
 }
